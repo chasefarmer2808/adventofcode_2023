635d4fce504a66bb3aa607164b7e0e8224680816
chasefarmer2808 <chasefarmer94@gmail.com>
2023-12-21 13:56:41 -0500

    day 5 part one complete

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,18 +1,12 @@
-use std::collections::HashMap;
 use std::hash::Hash;
+use std::ops::{ Range};
 use crate::utils::read_lines;
 
-pub fn part_one(input_file: &str) -> u64 {
+pub fn part_one(input_file: &str) -> i64 {
     let almanac_lines = read_lines(input_file);
-    let mut seeds: Vec<u64> = Vec::new();
-    let mut locs: Vec<u64> = Vec::new();
-    let mut seed_to_soil_map: HashMap<u64, u64> = HashMap::new();
-    let mut soil_to_fert_map: HashMap<u64, u64> = HashMap::new();
-    let mut fert_to_water_map: HashMap<u64, u64> = HashMap::new();
-    let mut water_to_light_map: HashMap<u64, u64> = HashMap::new();
-    let mut light_to_temp_map: HashMap<u64, u64> = HashMap::new();
-    let mut temp_to_humid_map: HashMap<u64, u64> = HashMap::new();
-    let mut humid_to_loc_map: HashMap<u64, u64> = HashMap::new();
+    let mut seeds: Vec<i64> = Vec::new();
+    let mut min = i64::MAX;
+    let mut mappings: Vec<Mapping> = Vec::new();
 
     // Parse Almanac
     for (i, line) in almanac_lines.iter().enumerate() {
@@ -22,80 +16,102 @@ pub fn part_one(input_file: &str) -> u64 {
         }
 
         if line.contains("seed-to-soil map:") {
-            let seed_to_soil_nums_strings: Vec<&String> = lines_to_num_strings(&almanac_lines[i+1..]);
-            let seed_to_soil_nums: Vec<Vec<u64>> = num_strings_to_num_vecs(seed_to_soil_nums_strings);
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&seed_to_soil_nums, &mut seed_to_soil_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("soil-to-fertilizer map:") {
-            let soil_to_fert_nums_strings: Vec<&String> = lines_to_num_strings(&almanac_lines[i+1..]);
-            let soil_to_fert_nums: Vec<Vec<u64>> = num_strings_to_num_vecs(soil_to_fert_nums_strings);
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&soil_to_fert_nums, &mut soil_to_fert_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("fertilizer-to-water map:") {
-            let fert_to_water_nums_strings: Vec<&String> = lines_to_num_strings(&almanac_lines[i+1..]);
-            let fert_to_water_nums: Vec<Vec<u64>> = num_strings_to_num_vecs(fert_to_water_nums_strings);
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&fert_to_water_nums, &mut fert_to_water_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("water-to-light map:") {
-            let nums: Vec<Vec<u64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&nums, &mut water_to_light_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("light-to-temperature map:") {
-            let nums: Vec<Vec<u64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&nums, &mut light_to_temp_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("temperature-to-humidity map:") {
-            let nums: Vec<Vec<u64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&nums, &mut temp_to_humid_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
 
         if line.contains("humidity-to-location map:") {
-            let nums: Vec<Vec<u64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
+            let mapping_nums: Vec<Vec<i64>> = num_strings_to_num_vecs(lines_to_num_strings(&almanac_lines[i+1..]));
 
-            parse_mappings(&nums, &mut humid_to_loc_map);
+            let mut mapping = Mapping::default();
+            for mapping_num in mapping_nums {
+                mapping.maps.push(create_mapping(mapping_num[0], mapping_num[1], mapping_num[2]))
+            }
+            mappings.push(mapping);
             continue;
         }
     }
 
     // Gather locations.
     for seed in &seeds {
-        let soil = get_mapping(&seed_to_soil_map, seed);
-        let fert = get_mapping(&soil_to_fert_map, &soil);
-        let water = get_mapping(&fert_to_water_map, &fert);
-        let light = get_mapping(&water_to_light_map, &water);
-        let temp = get_mapping(&light_to_temp_map, &light);
-        let humid = get_mapping(&temp_to_humid_map, &temp);
-        let loc = get_mapping(&humid_to_loc_map, &humid);
-
-        locs.push(loc);
+        println!("parsing seed {:?}", seed);
+        let mut cur = *seed;
+        for map in &mappings {
+            cur = map.apply_map(cur);
+        }
+        min = min.min(cur);
     }
 
-    return locs.iter().min().unwrap().clone();
+    return min;
 }
 
-fn parse_seeds(seed_line: &str) -> Vec<u64> {
+fn parse_seeds(seed_line: &str) -> Vec<i64> {
     seed_line
         .split_whitespace()
-        .filter(|s| s.parse::<u64>().is_ok())
-        .map(|s| s.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>()
+        .filter(|s| s.parse::<i64>().is_ok())
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>()
 }
 
 fn lines_to_num_strings(num_lines: &[String]) -> Vec<&String> {
@@ -105,43 +121,37 @@ fn lines_to_num_strings(num_lines: &[String]) -> Vec<&String> {
         .collect()
 }
 
-fn num_strings_to_num_vecs(num_strings: Vec<&String>) -> Vec<Vec<u64>> {
+fn num_strings_to_num_vecs(num_strings: Vec<&String>) -> Vec<Vec<i64>> {
     num_strings
         .iter()
         .map(|s| s.split_whitespace()
-            .map(|ns| ns.parse::<u64>().unwrap())
-            .collect::<Vec<u64>>())
-        .collect::<Vec<Vec<u64>>>()
+            .map(|ns| ns.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>())
+        .collect::<Vec<Vec<i64>>>()
 }
 
-fn parse_mappings(mappings: &Vec<Vec<u64>>, target_map: &mut HashMap<u64, u64>) {
-    for mapping in mappings {
-        if let [dest, src, r] = mapping[..] {
-            let mut j = 0;
+fn create_mapping(dest: i64, src: i64, r: i64) -> SingleMap {
+    SingleMap {range: src..src+r, delta: dest - src}
+}
 
-            for n in src..src+r {
-                target_map.insert(n, dest + j);
-                j += 1;
+#[derive(Debug, Default)]
+struct Mapping {
+    maps: Vec<SingleMap>
+}
+
+impl Mapping {
+    fn apply_map(&self, val: i64) -> i64 {
+        for map in &self.maps {
+            if map.range.contains(&val) {
+                return val + map.delta;
             }
         }
+        val
     }
 }
 
-fn get_mapping(mapping: &HashMap<u64, u64>, target: &u64) -> u64 {
-    match mapping.contains_key(target) {
-        true => mapping.get(target),
-        false => Some(target)
-    }.unwrap().clone()
-}
-
-#[derive(Debug)]
-struct Almanac {
-    seeds: Vec<u64>,
-    seed_to_soil: HashMap<u64, u64>,
-    soil_to_fert: HashMap<u64, u64>,
-    fert_to_water: HashMap<u64, u64>,
-    water_to_light: HashMap<u64, u64>,
-    light_to_temp: HashMap<u64, u64>,
-    temp_to_humid: HashMap<u64, u64>,
-    humid_to_loc: HashMap<u64, u64>
+#[derive(Debug, Default)]
+struct SingleMap {
+    range: Range<i64>,
+    delta: i64
 }
